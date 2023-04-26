ef80c8ec498ace21620775dd8a729be52b756256
Ivan Verdugo <ivan270@gmail.com>
2023-04-26 12:00:19 -0400

    simplified code by using a class for user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,31 @@
 let form = document.getElementById('formCuotas');
 
-let aPagar = '';
-let interes = '';
-let cuotasAPagar = '';
+class Cuota {
+	constructor(monto, interes, cuotas) {
+		this.monto = monto;
+		this.interes = interes;
+		this.cuotas = cuotas;
+		this.calcularTotal = () => this.monto * (this.interes / 100 + 1);
+		this.valorCuota = () => this.calcularTotal() / this.cuotas;
+	}
+}
 
 formCuotas.addEventListener('submit', (event) => {
 	event.preventDefault();
-	aPagar = cuantoPagar.value;
-	procesarDatos(cuantoInteres.value, cuantasCuotas.value);
-	formCuotas.reset();
+	let inputUsuario = new Cuota(
+		cuantoPagar.value,
+		cuantoInteres.value,
+		cuantasCuotas.value
+	);
+	let valorCuotaFormateada = formatearMoneda(
+		Math.ceil(inputUsuario.valorCuota())
+	);
+	insertarDatos(valorCuotaFormateada, inputUsuario.cuotas);
 });
-
-const procesarDatos = (interes, cuotas) => {
-	interes = calcularInteres(interes);
-	cuotasAPagar = cuotas;
-	totalAPagar = calcularTotalAPagar(aPagar, interes);
-	let valorCuota = calcularCuotas(totalAPagar, cuotasAPagar);
-	// Para que precio se aproxime al numero entero superior
-	insertarDatos(formatearMoneda(Math.ceil(valorCuota)), cuotasAPagar);
-};
 
 const insertarDatos = (montoCuotas, cantidadCuotas) => {
 	resultadoAPagar.innerHTML = `¡No te preocupes! Puedes pagarlo en <span class="font-bold text-6xl">${cantidadCuotas}</span> cuotas de <span class="font-bold text-6xl">${montoCuotas}</span>`;
 };
-const calcularInteres = (porcentaje) => porcentaje / 100 + 1;
-const calcularTotalAPagar = (monto, interesFormateado) => {
-	return monto * interesFormateado;
-};
-const calcularCuotas = (total, cuotas) => {
-	return total / cuotas;
-};
+
 const formatearMoneda = (num) =>
 	num.toLocaleString('es-CL', { style: 'currency', currency: 'clp' });
